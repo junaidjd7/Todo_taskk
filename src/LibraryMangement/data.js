a3f7453f74754ba1b81a5c707b034b13689a5859
junaidjd7 <96939916+junaidjd7@users.noreply.github.com>
2022-05-18 04:03:34 -0700

    librarry management system

---
+++ src/LibraryMangement/data.js
@@ -0,0 +1,45 @@
+export const data = [
+        {
+          id: 1,
+          BookName: 'science',
+          Author: 'jjbbvjv',
+          QualityofBook: 'dh',
+          publicationDate: 'kjjd',
+          Availability: 'bd',
+        },
+        {
+          id: 2,
+          BookName: 'isl',
+          Author: 'dj',
+          publicationDate: 'kjjd',
+          QualityofBook: 'dh',
+          Availability: 'ekoo',
+        },
+        {
+          id: 3,
+          BookName: 'eng',
+          Author: 'dj',
+          publicationDate: 'kjjd',
+          QualityofBook: 'dh',
+          Availability: 'ekoo',
+        },
+        {
+          id: 4,
+          BookName: 'urdu',
+          Author: 'dj',
+          publicationDate: 'kjjd',
+          QualityofBook: 'dh',
+          Availability: 'ekoo',
+        },
+        {
+          id: 5,
+          BookName: 'mathematics',
+          Author: 'dj',
+          publicationDate: 'kjjd',
+          QualityofBook: 'dh',
+          Availability: 'ekoo',
+        },
+      ];
+      
+
+ 
